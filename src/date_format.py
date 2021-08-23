32ef2b5c71e53c16b12b0bd1d938d3fb2eb61fc6
jalex141 <saiz30@gmail.com>
2021-08-23 11:52:01 +0200

    t

--- src/date_format.py
+++ src/date_format.py
@@ -1,7 +1,21 @@
+import re
 
 def date_f(item):
-    try:
-        len(item.split("-")) ==3
+    if len(item.split("-")) ==3:
         return item
-    except:
+    else:
+        return "date not complete"
+
+def get_year(item):
+    l=re.findall("(\d{4})", item)
+    if l:
+        return l[0]
+    else:
+        return "error"
+
+def date_r(item):
+    s = re.search("(\d+\-\w{3}\-\d+)",item)
+    if s:
+        return s[0]
+    else:
         return "date not complete"
--- src/moon_ph.py
+++ src/moon_ph.py
@@ -1,93 +1,68 @@
-def moon(a):
+import math
+
+#utilizando la fecha obtenemos la fase lunar
+def slit(item):
+    return item.split("-")
+
+def num_month(month):
+    if month == "Jan":
+        return 1
+    elif month == "Feb":
+        return 2
+    elif month == "Mar":
+        return 3
+    elif month == "Apr":
+        return 4
+    elif month == "May":
+        return 5
+    elif month == "Jun":
+        return 6
+    elif month == "Jul":
+        return 7
+    elif month == "Aug":
+        return 8
+    elif month == "Sep":
+        return 9
+    elif month == "Oct":
+        return 10
+    elif month == "Nov":
+        return 11
+    elif month == "Dec":
+        return 12
+
+def moon(item):
+    try:
+        date=item.split("-")
+        m=int(num_month(date[1]))
+        d=int(date[0]);  y=int(date[2]);
+
+        return conway(d,m,y)
+    except:
+        display(item)
 
 
+# This is based on a 'do it in your head' algorithm by John Conway.
+# In its current form, it's only valid for the 20th and 21st centuries
 
-    export const julDay = (day: number, month: number, year: number): number => {
-    let jy = year
-    let jm = month + 1
-
-    if (month <= 2) {
-        jy--
-        jm += 12
-    }
-    let jul = Math.floor(365.25 * jy) + Math.floor(30.6001 * jm) + day + 1720995
-    if (day + 31 * (month + 12 * year) >= 15 + 31 * (10 + 12 * 1582)) {
-        const ja = Math.floor(0.01 * jy)
-        jul = jul + 2 - ja + Math.floor(0.25 * ja)
-    }
-    return jul
-}
-
-export const getFrac = (fr: number): number => {
-    return fr - Math.floor(fr)
-
-
-    import { julDay, getFrac } from './utils'
-
-// This simply mods the difference between the date and a known new moon date (1970-01-07) by the length of the lunar period.
-// For this reason, it is only valid from 1970 onwards.
-export const Simple = (day: number, month: number, year: number): number => {
-    const lp = 2551443
-    const now = new Date(year, month - 1, day, 20, 35, 0)
-    const new_moon = new Date(1970, 0, 7, 20, 35, 0)
-    const phase = ((now.getTime() - new_moon.getTime()) / 1000) % lp
-    return Math.floor(phase / (24 * 3600))
-}
-
-// This is based on a 'do it in your head' algorithm by John Conway.
-// In its current form, it's only valid for the 20th and 21st centuries, but I'm sure John's got refinements. :)
-export const Conway = (day: number, month: number, year: number): number => {
-    let phase = year % 100
+def conway(day, month, year):
+    phase = year % 100
 
     phase %= 19
-    if (phase > 9) {
+    if phase > 9:
         phase -= 19
-    }
+    
 
     phase = ((phase * 11) % 30) + month + day
-    if (month < 3) {
+    if month < 3:
         phase += 2
-    }
+    
+    if year < 2000:
+        phase -= 4
+    else:
+        phase -= 8.3
+    phase = math.floor(phase + 0.5) % 30
 
-    phase -= year < 2000 ? 4 : 8.3
-    phase = Math.floor(phase + 0.5) % 30
+    if phase < 0:
+        phase + 30
+    return phase
 
-    return phase < 0 ? phase + 30 : phase
-}
-
-// This is based on some Basic code by Roger W. Sinnot from Sky & Telescope magazine, March 1985.
-// I don't pretend to understand it - something to do with a first-order approximation to the 'real' calculation of the position of the bodies involved
-export const Sinnot = (day: number, month: number, year: number): number => {
-    const thisJD = julDay(day, month, year)
-    const degToRad = 3.14159265 / 180
-    const K0: number = Math.floor((year - 1900) * 12.3685)
-    const T = (year - 1899.5) / 100
-    const T2 = T * T
-    const T3 = T * T * T
-    const J0 = 2415020 + 29 * K0
-    const F0 =
-        0.0001178 * T2 -
-        0.000000155 * T3 +
-        (0.75933 + 0.53058868 * K0) -
-        (0.000837 * T + 0.000335 * T2)
-    const M0 = 360 * getFrac(K0 * 0.08084821133) + 359.2242 - 0.0000333 * T2 - 0.00000347 * T3
-    const M1 = 360 * getFrac(K0 * 0.07171366128) + 306.0253 + 0.0107306 * T2 + 0.00001236 * T3
-    const B1 = 360 * getFrac(K0 * 0.08519585128) + 21.2964 - 0.0016528 * T2 - 0.00000239 * T3
-    let oldJ = 0
-    let phase = 0
-    let jDay = 0
-    while (jDay < thisJD) {
-        let F = F0 + 1.530588 * phase
-        const M5 = (M0 + phase * 29.10535608) * degToRad
-        const M6 = (M1 + phase * 385.81691806) * degToRad
-        const B6 = (B1 + phase * 390.67050646) * degToRad
-        F -= 0.4068 * Math.sin(M6) + (0.1734 - 0.000393 * T) * Math.sin(M5)
-        F += 0.0161 * Math.sin(2 * M6) + 0.0104 * Math.sin(2 * B6)
-        F -= 0.0074 * Math.sin(M5 - M6) - 0.0051 * Math.sin(M5 + M6)
-        F += 0.0021 * Math.sin(2 * M5) + 0.001 * Math.sin(2 * B6 - M6)
-        F += 0.5 / 1440
-        oldJ = jDay
-        jDay = J0 + 28 * phase + Math.floor(F)
-        phase++
-    }
-    return (thisJD - oldJ) % 30
